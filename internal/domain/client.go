069c62916f6078b037180fffccaa0d99edeb2c3f
ericolvr <ericolvr@gmail.com>
2026-05-23 12:05:59 -0300

    added enum

--- internal/domain/client.go
+++ internal/domain/client.go
@@ -8,20 +8,34 @@ import (
 
 var mailRegex = regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$`)
 
+type Status string
+
+const (
+	StatusAguardandoAnalise Status = "Aguardando Análise"
+	StatusProcessado        Status = "Processado"
+)
+
+type Priority string
+
+const (
+	PriorityAlta   Priority = "prioridade_alta"
+	PriorityNormal Priority = "prioridade_normal"
+)
+
 type Client struct {
-	ID            int64   `json:"id" db:"id"`
-	CustomerName  string  `json:"cliente_nome" db:"cliente_nome"`
-	CustomerEmail string  `json:"cliente_email" db:"cliente_email"`
-	RequestType   string  `json:"tipo_solicitacao" db:"tipo_solicitacao"`
-	AssetValue    float64 `json:"valor_patrimonio" db:"valor_patrimonio"`
-	Status        string  `json:"status" db:"status"`
-	Priority      string  `json:"prioridade" db:"prioridade"`
+	ID            int64     `json:"id" db:"id"`
+	CustomerName  string    `json:"cliente_nome" db:"cliente_nome"`
+	CustomerEmail string    `json:"cliente_email" db:"cliente_email"`
+	RequestType   string    `json:"tipo_solicitacao" db:"tipo_solicitacao"`
+	AssetValue    float64   `json:"valor_patrimonio" db:"valor_patrimonio"`
+	Status        Status    `json:"status" db:"status"`
+	Priority      *Priority `json:"prioridade" db:"prioridade"`
 }
 
 type ClientRepository interface {
 	Create(ctx context.Context, client *Client) error
 	FindByEmail(ctx context.Context, email string) (*Client, error)
-	UpdateStatus(ctx context.Context, email, status, priority string) error
+	UpdateStatus(ctx context.Context, email string, status Status, priority Priority) error
 }
 
 func (c *Client) Validate() error {
