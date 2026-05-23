03fef5b3b27e9a85d3e2a003db34bcb117d04a13
ericolvr <ericolvr@gmail.com>
2026-05-23 11:24:12 -0300

    versioned routes

---
+++ internal/infrastructure/http/routes.go
@@ -0,0 +1,18 @@
+package http
+
+import (
+	"github.com/ericolvr/ewz/internal/interfaces/api"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(
+	router *gin.Engine,
+	clientHandler *api.ClientHandler,
+	// webhookHandler *api.WebhookHandler,
+) {
+	v1 := router.Group("/api/v1")
+	{
+		v1.POST("/clientes", clientHandler.Create)
+		// v1.POST("/webhooks/pipefy/card-updated", webhookHandler.Process)
+	}
+}
